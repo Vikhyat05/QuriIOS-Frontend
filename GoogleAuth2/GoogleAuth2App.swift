dd2c64011beaa07bddf23f86ba6821203f7b3c0c
Lotus <vikhyatrocks@gmail.com>
2025-02-05 20:06:23 +0530

    Initial Commit

---
+++ GoogleAuth2/GoogleAuth2App.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleAuth2App.swift
+//  GoogleAuth2
+//
+//  Created by vikhyat rana on 05/02/25.
+//
+
+import SwiftUI
+
+@main
+struct GoogleAuth2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GoogleAuth2Tests/GoogleAuth2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleAuth2Tests.swift
+//  GoogleAuth2Tests
+//
+//  Created by vikhyat rana on 05/02/25.
+//
+
+import Testing
+@testable import GoogleAuth2
+
+struct GoogleAuth2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
